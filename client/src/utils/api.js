b15c63af75b36bcacb790d86364e83adc127f013
anushastefyj <anushastefy002@gmail.com>
2026-09-24 06:34:39 +0530

    Update production API URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://skillproof-u94g.onrender.com/api',
+  baseURL: 'https://skillproof-u94g.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
